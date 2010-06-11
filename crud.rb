377505783438cd7cdedab9b3c401574c9c6f780b
= <=>
2010-06-11 09:59:42 +0100

    todo

--- crud.rb
+++ crud.rb
@@ -2,22 +2,27 @@
 require 'rubygems'
 require 'sinatra' # should be the first line
 require 'active_record'
+
 configure do
 # connect to the database
 dbconfig = YAML.load(File.read('config/database.yml'))
 puts dbconfig
 ActiveRecord::Base.establish_connection dbconfig['production']
-begin
-ActiveRecord::Schema.define do
-create_table :students do |t|
-t.string :name, :null => false, :limit => 100
-t.string :email, :null => false, :limit => 50
-end
-end
+	begin
+	ActiveRecord::Schema.define do
+		create_table :students do |t|
+		t.string :name, :null => false, :limit => 100
+		t.string :email, :null => false, :limit => 50
+		end
+	end
+
 rescue ActiveRecord::StatementInvalid
 # do nothing - gobble up the error
 end
+
 end
+
+
 # define a simple model
 class Student < ActiveRecord::Base
 end
@@ -28,6 +33,8 @@ erb :new
 # Contd. from previous page
 end
 # Save the participant's info
+
+
 post '/' do
 begin
 @student = Student.new(:name => params[:name], :email =>
@@ -38,6 +45,8 @@ rescue
 redirect '/'
 end
 end
+
+
 # Display participant's details
 get '/:id' do
 begin
